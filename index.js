cc7a3af26e32bee4d27bff9fdb1647db70b7c496
CookieAlien <allen37751@gmail.com>
2023-10-25 13:02:53 +0800

    multi target test

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const exampleTarget = document.querySelector('#example-target')
-const exampleImage = document.querySelector('#example-image')
-const messageBtn = document.querySelector('#message-btn')
-console.log(exampleTarget);
+const exampleTarget = document.querySelector("#example-target")
+const exampleImage = document.querySelector("#example-image")
+const messageBtn = document.querySelector("#message-btn")
+console.log(exampleTarget)
 localStorage.clear()
-exampleTarget.addEventListener("targetFound", event => {
-    console.log("target found");
-    scanned = localStorage.getItem("scanned")
-    if(scanned){
-        messageBtn.textContent = "You have already scanned this item."
-        messageBtn.style.display = 'block'
-    }else{
-        localStorage.setItem("scanned","true")
-        messageBtn.textContent = "You've scanned a new item!"
-        messageBtn.style.display = 'block'
-    }
-  });
-messageBtn.addEventListener("click",event =>{
-    messageBtn.style.display = 'none'
+exampleTarget.addEventListener("targetFound", (event) => {
+  console.log("target found")
+  scanned = localStorage.getItem("scanned")
+  if (scanned) {
+    messageBtn.textContent = "You have already scanned this item."
+    messageBtn.style.display = "block"
+  } else {
+    localStorage.setItem("scanned", "true")
+    messageBtn.textContent = "You've scanned a new item!"
+    messageBtn.style.display = "block"
+  }
 })
-exampleTarget.addEventListener("targetLost", event => {
-    console.log("target lost");
-  });
+messageBtn.addEventListener("click", (event) => {
+  messageBtn.style.display = "none"
+})
+exampleTarget.addEventListener("targetLost", (event) => {
+  console.log("target lost")
+})
 
-exampleImage.addEventListener("click", event => {
-    console.log("image click");
-    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank').focus();
+exampleImage.addEventListener("click", (event) => {
+  console.log("image click")
+  window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank").focus()
 })
